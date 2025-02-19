26119e94a367f335aeb480236f6bcdf0a5d640cc
sysproxy <sysproxy64@gmail.com>
2025-02-19 09:28:36 +0100

    fix: update dependencies to match the target host

--- install.py
+++ install.py
@@ -2,7 +2,7 @@ from sentence_transformers import SentenceTransformer
 
 import constants
 
-model = SentenceTransformer(f'sentence-transformers/{constants.MODEL_NAME}')
+model = SentenceTransformer(f'sentence-transformers/{constants.MODEL_NAME}', device='cpu')
 model.save(constants.MODEL_PATH)
 
 print(f'Model saved to {constants.MODEL_PATH}')
--- release.py
+++ release.py
@@ -5,7 +5,7 @@ from sentence_transformers import SentenceTransformer
 
 import constants
 
-model = SentenceTransformer(constants.MODEL_PATH)
+model = SentenceTransformer(constants.MODEL_PATH, device='cpu')
 
 with open(os.path.join(constants.RELEASE_DIR, 'model.pkl'), 'wb') as f:
     pickle.dump(model, f)
